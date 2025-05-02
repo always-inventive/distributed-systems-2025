b2c67c497f4bfcd0d7ba70913784d95804866829
Leonidas Papadopoulos <lpapadopoulos@profilesw.com>
2025-05-02 14:41:42 +0300

    Add StoreDataRequest.

---
+++ src/gr/aueb/dtos/StoreDataRequest.java
@@ -0,0 +1,19 @@
+package gr.aueb.dtos;
+
+import gr.aueb.model.Store;
+
+/**
+ * Request to store/update data for a whole store.
+ */
+public class StoreDataRequest extends BaseRequest {
+    private static final long serialVersionUID = 101L;
+    private final Store store; // Send the whole Store object
+
+    public StoreDataRequest(Store store) {
+        this.store = store;
+    }
+
+    public Store getStore() {
+        return store;
+    }
+}
