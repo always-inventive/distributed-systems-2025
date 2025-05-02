0803aa6fcd11c97038cf3b4f4bd9b65d468230a4
Leonidas Papadopoulos <lpapadopoulos@profilesw.com>
2025-05-02 13:59:52 +0300

    Add BaseRequest and BaseResponse.

---
+++ src/gr/aueb/dtos/BaseRequest.java
@@ -0,0 +1,10 @@
+package gr.aueb.dtos;
+
+import java.io.Serializable;
+
+/**
+ * Base class for all request DTOs.
+ */
+public abstract class BaseRequest implements Serializable {
+    private static final long serialVersionUID = 10L;
+}
---
+++ src/gr/aueb/dtos/BaseResponse.java
@@ -0,0 +1,10 @@
+package gr.aueb.dtos;
+
+import java.io.Serializable;
+
+/**
+ * Base class for all response DTOs.
+ */
+public abstract class BaseResponse implements Serializable {
+    private static final long serialVersionUID = 20L;
+}
